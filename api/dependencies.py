34cb5c8bb3c85b62056fc8ee00e70921cb5ef806
shritish20 <shritish@amityonline.com>
2025-11-26 17:55:49 +0530

    Update dependencies.py

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,1 +1,11 @@
+from core.engine import VolGuardHybridUltimate
 
+# Global engine instance
+engine: VolGuardHybridUltimate = None
+
+def get_engine() -> VolGuardHybridUltimate:
+    return engine
+
+def set_engine(new_engine: VolGuardHybridUltimate):
+    global engine
+    engine = new_engine
