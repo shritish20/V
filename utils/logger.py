af172b9faf8d7d8285139b95689aec2c391e9888
shritish20 <shritish@amityonline.com>
2025-11-27 07:36:12 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,14 @@
-import logging
+import logging 
 import os
 from core.config import TRADE_LOG_FILE
 
 class ColoredFormatter(logging.Formatter):
     COLORS = {
-        'DEBUG': '\x1b[38;20m', 'INFO': '\x1b[38;20m',
-        'WARNING': '\x1b[33;20m', 'ERROR': '\x1b[31;20m', 'CRITICAL': '\x1b[31;1m'
+        'DEBUG': '\x1b[38;20m',
+        'INFO': '\x1b[38;20m',
+        'WARNING': '\x1b[33;20m',
+        'ERROR': '\x1b[31;20m',
+        'CRITICAL': '\x1b[31;1m'
     }
     RESET = '\x1b[0m'
     
@@ -18,14 +21,15 @@ def setup_logger():
     """Setup enhanced logging"""
     logger = logging.getLogger("VolGuardHybrid")
     logger.setLevel(logging.INFO)
-
+    
     fh = logging.FileHandler(TRADE_LOG_FILE)
     fh.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-
+    
     sh = logging.StreamHandler()
     sh.setFormatter(ColoredFormatter('%(asctime)s - %(levelname)s - %(message)s'))
-
-    logger.addHandler(fh)
-    logger.addHandler(sh)
     
+    if not logger.handlers:
+        logger.addHandler(fh)
+        logger.addHandler(sh)
+        
     return logger
